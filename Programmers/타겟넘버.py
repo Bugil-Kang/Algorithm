d198cdf321d97fc27d27a27cebc51fdf1dbb78e5
Bugil-Kang <43834870+Bugil-Kang@users.noreply.github.com>
2021-06-21 20:18:46 +0900

    Update 타겟넘버.py

--- Programmers/타겟넘버.py
+++ Programmers/타겟넘버.py
@@ -1,31 +1,19 @@
-def dist(num1, num2):
-    if num1 > num2:
-        return num1 - num2
-    else:
-        return num2 - num1
-
-
 def solution(numbers, target):
     answer = 0
-    current = 0
-    queue = []
-    queue.append([0, 0])
-    length = len(numbers)
+    length = len(numbers)  ## List 길이 구함
+    twice = [1]
+    for idx in range(length+1):
+        twice.append(twice[idx]*2)
 
-    for number in numbers:
-        while (len(queue) != 0 and queue[0][1] == current):
-            queue_list = queue.pop(0)
-            new_list1 = [queue_list[0] + number, queue_list[1] + 1]
-            new_list2 = [queue_list[0] - number, queue_list[1] + 1]
-            if new_list1[1] == len(numbers) - 1:
-                if dist(new_list1[0], target) == numbers[length - 1]:
-                    answer += 1
-                if dist(new_list2[0], target) == numbers[length - 1]:
-                    answer += 1
-            else:
-                queue.append(new_list1)
-                queue.append(new_list2)
+    num_list = [0, 0]
+    for idx in range(length):
+        number = numbers[idx]
+        for bin in range(twice[idx],twice[idx+1]):
+            num_list.append(num_list[bin]-number)
+            num_list.append(num_list[bin]+number)
 
-        current += 1
+    for idx in range(twice[length], twice[length+1]):
+        if num_list[idx] == target:
+            answer += 1
 
     return answer
